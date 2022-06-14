be0aeee55b246be985ab4267abe30bc51dc47027
daraivanova <kudiyarova.1975@gmail.com>
2022-06-14 07:20:26 +0300

    fit: Добавлен Interceptor

---
+++ src/app/core/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+   const req = request.clone({
+     headers
+   })
+    return next.handle(req);
+  }
+}
